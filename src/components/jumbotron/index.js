ba20aa6d4e44390ba8438b2fa8ccde3e80236902
nataliepina <natalie.a.pina@gmail.com>
2021-06-23 09:18:09 -0400

    create jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Container, Inner, Item } from './styles/jumbotron';
+import { Container, Image, Inner, Item, Pane, SubTitle, Title } from './styles/jumbotron';
 
-const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
-  return (
-    <Item {...restProps}>
-      <Inner direction={direction}>{children}</Inner>
-    </Item>
-  )
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+    return (
+        <Item {...restProps}>
+            <Inner direction={direction}>{children}</Inner>
+        </Item>
+    )
 }
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 }
 
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+}
 
-export default Jumbotron;
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps } />;
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+}
